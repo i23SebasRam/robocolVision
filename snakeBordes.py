49a54618ccb061c231622ccd64d013df7bb01e0a
i23SebasRam <80076520+i23SebasRam@users.noreply.github.com>
2021-04-06 23:05:42 -0500

    Se agregan las imagenes

---
+++ snakeBordes.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+#Imagen 1, toda la imagen es igual (Verde).
+img1 = cv.imread('C:/Users/pc/OneDrive - Universidad de los andes/Septimo semestre/Robocol/Img/Imagen7.jpg')
+#Imagen 2, tiene formas interesantes para probar como funciona el algoritmo.
+img2 = cv.imread('C:/Users/pc/OneDrive - Universidad de los andes/Septimo semestre/Robocol/Img/Imagen8.jpg')
+#Imagen 3, es una imagen modelo de lo que veria el robert.
+img3 = cv.imread('C:/Users/pc/OneDrive - Universidad de los andes/Septimo semestre/Robocol/Img/cosita2.jpg')
+
