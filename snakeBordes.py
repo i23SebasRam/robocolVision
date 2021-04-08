c6fbdab0ae6e721d231ba553a76414a1944c08b6
i23SebasRam <80076520+i23SebasRam@users.noreply.github.com>
2021-04-08 13:33:48 -0500

    Se agrega una funcion que calcula las distancias

--- snakeBordes.py
+++ snakeBordes.py
@@ -9,3 +9,21 @@ img2 = cv.imread('C:/Users/pc/OneDrive - Universidad de los andes/Septimo semest
 #Imagen 3, es una imagen modelo de lo que veria el robert.
 img3 = cv.imread('C:/Users/pc/OneDrive - Universidad de los andes/Septimo semestre/Robocol/Img/cosita2.jpg')
 
+#Vamos a hacer una funcion que revise la distancia euclidiana de los tres canales.
+def revisoParecido(seccionActual,seccionComparar,mode = 0):
+    dis = 0
+    promActu = cv.mean(seccionActual)
+    promComp = cv.mean(seccionComparar)
+    #Distancia euclidiana.
+    if mode == 0:
+        disEucli = ((promActu[0]- promComp[0])^2 + (promActu[1]- promComp[1])^2 +(promActu[2]- promComp[2])^2 )^(1/2)
+        dis = disEucli
+    #RMSE penaliza distancias lejanas
+    if mode == 1:
+        RMSE = (((promActu[0]- promComp[0])^2 + (promActu[1]- promComp[1])^2 +(promActu[2]- promComp[2])^2)/3 )^(1/2)
+        dis = RMSE
+    #Distancia manhattan.
+    if mode == 2:
+        disMan = abs(promActu[0]- promComp[0]) + abs(promActu[1]- promComp[1]) + abs(promActu[2]- promComp[2]) 
+        dis = disMan
+    return dis
