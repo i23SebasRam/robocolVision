0dc53fcc921a00048ead05215fccc8c7d769a7c3
i23SebasRam <80076520+i23SebasRam@users.noreply.github.com>
2021-04-08 14:35:30 -0500

    Se agrega una funcion master y en ella se rescalan las imagenes si es necesario

--- snakeBordes.py
+++ snakeBordes.py
@@ -11,6 +11,25 @@ img2 = cv.imread('C:/Users/pc/OneDrive - Universidad de los andes/Septimo semest
 #Imagen 3, es una imagen modelo de lo que veria el robert.
 img3 = cv.imread('C:/Users/pc/OneDrive - Universidad de los andes/Septimo semestre/Robocol/Img/cosita2.jpg')
 
+def master(img,divFilas = 10,divColum = 10):
+    dim = img.shape
+    #Se hace un escalamiento para que la imagen se pueda dividir en partes iguales.
+    if dim[0] % divFilas != 0:
+        fil = int(dim[0]/divFilas) * divFilas
+        img = cv.resize(img,(fil,dim[1]))
+    elif dim[1] % divColum != 0:
+        col = int(dim[1]/divColum) * divColum
+        img = cv.resize(img,(dim[0],col))
+    
+
+
+#Funcion que recibe los indices y entrega la imagen pequeñita.
+#ind, un vector fila donde el primer valor es la esquina izquierda abajo y va en sentido horario del cuadrado.
+def pasameIndices(imagen,ind):
+    return imagen[ind[1]:ind[0],ind[2]:ind[3]]
+    
+
+
 #Vamos a hacer una funcion que revise la distancia euclidiana de los tres canales.
 def revisoParecido(seccionActual,seccionComparar,mode = 0):
     dis = 0
@@ -30,5 +49,6 @@ def revisoParecido(seccionActual,seccionComparar,mode = 0):
         dis = disMan
     return dis
 
-prom = revisoParecido(img1,img2,mode=1)
-print(prom)
+imgen = pasameIndices(img1,np.array([630,610,300,630]))
+cv.imshow("imagen",imgen)
+
