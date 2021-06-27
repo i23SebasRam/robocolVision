f33a5fa0eec72f7247135c534e833fdad69c4713
i23SebasRam <80076520+i23SebasRam@users.noreply.github.com>
2021-06-26 22:51:56 -0500

    Correction of an issues

--- Segmentation/augmentation.py
+++ Segmentation/augmentation.py
@@ -21,23 +21,20 @@ listNamesX = os.listdir(pathx)
 listNamesY = os.listdir(pathy)
 
 #Horizontal and vertical shift
-def Shift(imagex,imagey,number,showImage = False,widthShift = 0.05,heightShift = 0.05):
+def Shift(imagex,imagey,number,post,showImage = False,widthShift = 0.05,heightShift = 0.05):
     datax = img_to_array(imagex)
     datay = img_to_array(imagey)
     samplesx = expand_dims(datax,0)
     samplesy = expand_dims(datay,0)
     datagen = ImageDataGenerator(width_shift_range=widthShift,height_shift_range=heightShift,fill_mode="wrap")
-    itx = datagen.flow(samplesx,batch_size=1,seed=42,save_to_dir="D:/testAugmen/xAugmented",save_prefix='x')
-    ity = datagen.flow(samplesy,batch_size=1,seed=42,save_to_dir="D:/testAugmen/yAugmented",save_prefix='y')
+    itx = datagen.flow(samplesx,batch_size=1,seed=42,save_to_dir="D:/testAugmen/xAugmented",save_prefix='x%s'%post)
+    ity = datagen.flow(samplesy,batch_size=1,seed=42,save_to_dir="D:/testAugmen/yAugmented",save_prefix='y%s'%post)
     for i in range(number):
         batchx = itx.next()
         batchy = ity.next()
         imagex = batchx[0].astype('uint8')
         imagey = batchy[0].astype('uint8')
         if showImage == True:
-            #pyplot.subplot(330+1+i)
-            #pyplot.imshow(imagey)
-            #pyplot.imshow(imagex)
             fig = pyplot.figure()
             ax1 = fig.add_subplot(1,2,1)
             ax2 = fig.add_subplot(1,2,2)
@@ -45,14 +42,14 @@ def Shift(imagex,imagey,number,showImage = False,widthShift = 0.05,heightShift =
             ax2.imshow(imagey)
     pyplot.show()
 #Horizontal and vertical flip
-def flip(imagex,imagey,number,showImage = False, horizontalFlip = True, verticalFlip = True, rotation = 10):
+def flip(imagex,imagey,number,post,showImage = False, horizontalFlip = True, verticalFlip = True, rotation = 10):
     datax = img_to_array(imagex)
     datay = img_to_array(imagey)
     samplesx = expand_dims(datax,0)
     samplesy = expand_dims(datay,0)
     datagen = ImageDataGenerator(horizontal_flip = horizontalFlip, vertical_flip = verticalFlip, rotation_range= rotation)
-    itx = datagen.flow(samplesx,batch_size=1,seed=42)
-    ity = datagen.flow(samplesy,batch_size=1,seed=42)
+    itx = datagen.flow(samplesx,batch_size=1,seed=42,save_to_dir="D:/testAugmen/xAugmented",save_prefix='x%s'%post)
+    ity = datagen.flow(samplesy,batch_size=1,seed=42,save_to_dir="D:/testAugmen/yAugmented",save_prefix='y%s'%post)
     for i in range(number):
         batchx = itx.next()
         batchy = ity.next()
@@ -66,7 +63,7 @@ def flip(imagex,imagey,number,showImage = False, horizontalFlip = True, vertical
             ax2.imshow(imagey)
     pyplot.show()
 #Change the brightness
-def brightness(imagex,imagey,number,showImage = False, rotation = False,rotationRange = 5):
+def brightness(imagex,imagey,number,post,showImage = False, rotation = False,rotationRange = 5):
     datax = img_to_array(imagex)
     datay = img_to_array(imagey)
     samplesx = expand_dims(datax,0)
@@ -77,8 +74,8 @@ def brightness(imagex,imagey,number,showImage = False, rotation = False,rotation
     datageny = ImageDataGenerator()
     #Range less - 1->Darken image. 
     #More - 1 -> Brighten image.
-    itx = datagen.flow(samplesx,batch_size=1,seed=42)
-    ity = datageny.flow(samplesy,batch_size=1,seed=42)
+    itx = datagen.flow(samplesx,batch_size=1,seed=42,save_to_dir="D:/testAugmen/xAugmented",save_prefix='x%s'%post)
+    ity = datageny.flow(samplesy,batch_size=1,seed=42,save_to_dir="D:/testAugmen/yAugmented",save_prefix='y%s'%post)
     for i in range(number):
         batchx = itx.next()
         batchy = ity.next()
@@ -92,7 +89,7 @@ def brightness(imagex,imagey,number,showImage = False, rotation = False,rotation
             ax2.imshow(imagey)
     pyplot.show()
 #Do zoom in or zoom out
-def zoom(imagex,imagey,number,showImage = False, zoomRange = [0.5,1.5], zoomIn = False, zoomOut=False):
+def zoom(imagex,imagey,number,post,showImage = False, zoomRange = [0.5,1.5], zoomIn = False, zoomOut=False):
     datax = img_to_array(imagex)
     datay = img_to_array(imagey)
     samplesx = expand_dims(datax,0)
@@ -107,8 +104,8 @@ def zoom(imagex,imagey,number,showImage = False, zoomRange = [0.5,1.5], zoomIn =
         datageny = ImageDataGenerator(zoom_range=[1.05,1.5])  
     #Range less - 1->Zoom in. 
     #More - 1 -> Zoom out.
-    itx = datagen.flow(samplesx,batch_size=1,seed=42)
-    ity = datageny.flow(samplesy,batch_size=1,seed=42)
+    itx = datagen.flow(samplesx,batch_size=1,seed=42,save_to_dir="D:/testAugmen/xAugmented",save_prefix='x%s'%post)
+    ity = datageny.flow(samplesy,batch_size=1,seed=42,save_to_dir="D:/testAugmen/yAugmented",save_prefix='y%s'%post)
     for i in range(number):
         batchx = itx.next()
         batchy = ity.next()
@@ -122,15 +119,15 @@ def zoom(imagex,imagey,number,showImage = False, zoomRange = [0.5,1.5], zoomIn =
             ax2.imshow(imagey)
     pyplot.show()
 #All the augmentations
-def generalAugmentation(imagex,imagey,number,showImage = False,horizontalFlip=False,verticalFlip=False,zoomRange=[1,1],bright = [1,1],rotation = 0):
+def generalAugmentation(imagex,imagey,number,post,showImage = False,horizontalFlip=False,verticalFlip=False,zoomRange=[1,1],bright = [1,1],rotation = 0):
     datax = img_to_array(imagex)
     datay = img_to_array(imagey)
     samplesx = expand_dims(datax,0)
     samplesy = expand_dims(datay,0)
-    datagen = ImageDataGenerator(horizontal_flip=horizontalFlip,vertical_flip=verticalFlip,zoom_range=zoomRange,brightness_range=bright,rotation_range=rotation)
-    datageny = ImageDataGenerator(horizontal_flip=horizontalFlip,vertical_flip=verticalFlip,zoom_range=zoomRange,rotation_range=rotation)
-    itx = datagen.flow(samplesx,batch_size=1,seed=42)
-    ity = datageny.flow(samplesy,batch_size=1,seed=42)
+    datagen = ImageDataGenerator(horizontal_flip=horizontalFlip,vertical_flip=verticalFlip,zoom_range=zoomRange,brightness_range=bright,rotation_range=rotation,fill_mode="wrap")
+    datageny = ImageDataGenerator(horizontal_flip=horizontalFlip,vertical_flip=verticalFlip,zoom_range=zoomRange,rotation_range=rotation,fill_mode="wrap")
+    itx = datagen.flow(samplesx,batch_size=1,seed=42,save_to_dir="D:/testAugmen/xAugmented",save_prefix='x%s'%post)
+    ity = datageny.flow(samplesy,batch_size=1,seed=42,save_to_dir="D:/testAugmen/yAugmented",save_prefix='y%s'%post)
     for i in range(number):
         batchx = itx.next()
         batchy = ity.next()
@@ -144,23 +141,22 @@ def generalAugmentation(imagex,imagey,number,showImage = False,horizontalFlip=Fa
             ax2.imshow(imagey)
     pyplot.show()
 #Load the image from a file
-def loadImage(listNameX,listNameY,numberI=4,mode = "general_augmentation",showImage = False):
+def loadImage(listNameX,listNameY,showImageO,numberI=5,mode = "general_augmentation"):
     listName = np.array([listNameX,listNameY])
     idx = listName.shape
     for i in range(idx[1]):
         rutax = os.path.join("D:\\testAugmen","x",listName[0,i])
-        print(rutax)
         rutay = os.path.join("D:\\testAugmen","y",listName[1,i])
         imgx = load_img(rutax)
         imgy = load_img(rutay)
         if mode == "shift":
-            Shift(imgx,imgy,number=numberI,showImage = False)
+            Shift(imgx,imgy,number=numberI,showImage = showImageO,post=i)
         elif mode == "shift":
-            flip(imgx,imgy,number=numberI)
+            flip(imgx,imgy,number=numberI,showImage = showImageO,post=i)
         elif mode == "shift":
-            brightness(imgx,imgy,number=numberI)
+            brightness(imgx,imgy,number=numberI,showImage = showImageO,post=i)
         elif mode == "shift":
-            zoom(imgx,imgy,number=numberI)
+            zoom(imgx,imgy,number=numberI,showImage = showImageO,post=i)
         else:
             generalAugmentation(
             imgx,
@@ -170,11 +166,12 @@ def loadImage(listNameX,listNameY,numberI=4,mode = "general_augmentation",showIm
             verticalFlip=True,
             zoomRange=[0.5,0.95], 
             bright=[0.5,1.5],
-            rotation=5
+            rotation=5,
+            showImage = showImageO,
+            post=i
             )
             
-
-loadImage(listNamesX,listNamesY,numberI=2,mode = "shift",showImage= True)
+loadImage(listNamesX,listNamesY,numberI=100,mode = "shift",showImageO= True)
 
 
 
